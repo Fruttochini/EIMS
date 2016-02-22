0be51ea99e43e443c8dae84ac3a7ea99c5d8fc21
Oleksandr Danylenko <oldan88@gmail.com>
2016-02-22 17:28:35 +0200

    Identity User updated - added other properties

--- EIMS.AuthorizationIdentity/IdentityUser.cs
+++ EIMS.AuthorizationIdentity/IdentityUser.cs
@@ -22,7 +22,17 @@ namespace EIMS.AuthorizationIdentity
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime LastEditDate { get; set; }
+        public DateTime? LastEditDate { get; set; }
+
+        public Nullable<byte> RoleID { get; set; }
+        //public System.DateTime CreationDate { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public string photoLink { get; set; }
+        public string address { get; set; }
+        public bool sex { get; set; }
+        //public DateTime? LastEditDate { get; set; }
 
     }
 }
