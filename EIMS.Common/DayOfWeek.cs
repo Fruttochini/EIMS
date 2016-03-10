03bb211d76f3a6a3b0424f9192852f09ca7ca2e7
zanpaln <zanpaln@i.ua>
2016-03-10 18:21:04 +0200

    Common Update

---
+++ EIMS.Common/DayOfWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Common
+{
+	public class DayOfWeek
+	{
+		public int DayID { get; set; }
+		public string DayName { get; set; }
+
+		public IEnumerable<long> LessonID { get; set; }
+	}
+}
---
+++ EIMS.Common/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMS.Common
+{
+	public class Task
+	{
+		public long taskID { get; set; }
+		public long lessonDateID { get; set; }
+		public string homeTask { get; set; }
+		public DateTime expiryDate { get; set; }
+	}
+}
