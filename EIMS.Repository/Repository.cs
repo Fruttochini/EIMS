7303ccb03022f08e8302a72d9aa3df0c7a5f491a
Oleksandr <danolex@ukr.net>
2016-03-10 14:06:26 +0200

    repository

--- EIMS.Repository/Repository.cs
+++ EIMS.Repository/Repository.cs
@@ -30,188 +30,63 @@ namespace EIMS.Repository
 
         public IEnumerable<Common.Room> GetRooms()
         {
-            throw new NotImplementedException();
+            var dblst = context.Room.ToList();
+            var result = new List<Common.Room>();
+
+            return result;
         }
 
-        public IEnumerable<Student> GetStudents()
-        {
-            throw new NotImplementedException();
-        }
+
 
         public IEnumerable<Common.Subject> GetSubjects()
         {
-            throw new NotImplementedException();
-        }
-
-        public Teacher GetTeacherByID(long ID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Teacher> GetTeachers()
-        {
-            throw new NotImplementedException();
-        }
-
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
+            var dbLst = context.Subject.ToList();
+            var result = new List<Common.Subject>();
+            foreach (var item in dbLst)
             {
-                if (disposing)
+                var tmpSubj = new Common.Subject() { SubjectID = item.subjectID, SubjectName = item.subjectName };
+                int arrLength = item.requirements.Length;
+                var reqList = new bool[arrLength];
+                for (int i = 0; i < arrLength; i++)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    reqList[i] = item.requirements[i] == 1 ? true : false;
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
+                tmpSubj.Requirements = reqList;
+                result.Add(tmpSubj);
             }
+            return result;
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~Repository() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
 
-        // This code added to correctly implement the disposable pattern.
+        public IEnumerable<Common.UniversityGroup> GetGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Common.Lesson> GetLessons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Common.Lesson> GetLessonsByDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+            throw new NotImplementedException();
         }
-        #endregion
 
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
 
-
-        //public void Dispose()
-        //{
-        //    context.Dispose();
-        //}
-
-
-
-        //public IEnumerable<Common.Course> GetCourses()
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public IEnumerable<Common.Faculty> GetFaculties()
-        //{
-        //    var DBFacultyList = context.Faculty.ToList();
-        //    var FacultyList = new List<Common.Faculty>();
-
-        //    foreach (var item in DBFacultyList)
-        //    {
-        //        var tmpf = new Common.Faculty()
-        //        {
-        //            FacultyID = item.facultyID,
-        //            Name = item.Name
-        //        };
-        //        FacultyList.Add(tmpf);
-        //    }
-
-        //    return FacultyList;
-        //}
-
-
-
-        //public IEnumerable<Common.Room> GetRooms()
-        //{
-        //    var DBRoomsList = context.Room.ToList();
-        //    var RoomsList = new List<Common.Room>();
-
-        //    foreach (var item in DBRoomsList)
-        //    {
-        //        var room = new Common.Room()
-        //        {
-        //            ID = item.roomID,
-        //            RoomNo = item.roomNo,
-        //            Capacity = item.capacity,
-        //            Features = item.features,
-        //            IsAvailable = item.isAvailable
-        //        };
-        //        RoomsList.Add(room);
-        //    }
-        //    return RoomsList;
-        //}
-
-        //public IEnumerable<Common.Student> GetStudents()
-        //{
-        //    var DBStudentList = context.GetUsersByRole(4);
-        //    var StudentList = new List<Common.Student>();
-
-        //    foreach (var item in DBStudentList)
-        //    {
-        //        var stud = new Student()
-        //        {
-        //            ID = item.ID,
-        //            Username = item.Username,
-        //            Name = item.Name,
-        //            Surname = item.Surname,
-        //            MiddleName = item.MiddleName,
-        //            Email = item.Email,
-        //            Password = item.Password,
-        //            address = item.address,
-        //            RoleID = item.RoleID,
-        //            CreationDate = item.CreationDate,
-        //            LastEditDate = item.LastEdit,
-        //            photoLink = item.photoLink,
-        //            sex = item.sex
-        //        };
-
-        //        StudentList.Add(stud);
-        //    }
-
-        //    return StudentList;
-        //}
-
-        //public IEnumerable<Common.Subject> GetSubjects()
-        //{
-        //    var SubjectList = new List<Common.Subject>();
-        //    var DBSubjectList = context.Subject.ToList();
-
-        //    foreach (var item in DBSubjectList)
-        //    {
-        //        Common.Subject subj = new Common.Subject()
-        //        {
-        //            SubjectID = item.subjectID,
-        //            SubjectName = item.subjectName,
-        //            Requirements = item.requirements
-        //        };
-
-        //        SubjectList.Add(subj);
-        //    }
-
-        //    return SubjectList;
-        //}
-
-        //public Teacher GetTeacherByID(long ID)
-        //{
-        //    var tmpTeacher = context.GetUsersByRole(3).Where(t => t.ID == ID).First();
-        //    return tmpTeacher.ToTeacher();
-        //}
-
-        //public IEnumerable<Common.Teacher> GetTeachers()
-        //{
-        //    var DBTeacherList = context.GetUsersByRole(3);
-        //    List<Teacher> TeacherList = new List<Teacher>();
-        //    foreach (var item in DBTeacherList)
-        //    {
-        //        Teacher tmpTeacher = item.ToTeacher();
-        //        TeacherList.Add(tmpTeacher);
-        //    }
-        //    return TeacherList;
-
-        //}
-
-
+        public User GetUserByID(long ID)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
