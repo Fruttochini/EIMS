00b701ba93b2809eb7dee3fa0054cc9c3a2232a6
zanpaln <zanpaln@i.ua>
2016-03-11 19:27:51 +0200

    Update

---
+++ EIMS/Models/FacultyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EIMS.Models
+{
+	public class FacultyViewModel
+	{
+		public int FacultyID { get; set; }
+		public string Name { get; set; }
+
+
+	}
+}
