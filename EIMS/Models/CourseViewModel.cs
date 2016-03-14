a1a32ddccbede49ba83c99df840b9d414fdb9257
zanpaln <zanpaln@i.ua>
2016-03-14 19:47:13 +0200

    CourseController, CourseViewModel

---
+++ EIMS/Models/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EIMS.Models
+{
+	public class CourseViewModel
+	{
+		public int CourseID { get; set; }
+		public string CourseName { get; set; }
+
+		public Dictionary<int,int> SubjectByHours { get; set; } 
+	}
+}
