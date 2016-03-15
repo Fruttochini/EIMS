0a8aef13dc06f434ed0274dd155274c58d6ad068
Oleksandr <danolex@ukr.net>
2016-03-15 10:24:09 +0200

    Faculty Controller

--- EIMS/Controllers/FacultyController.cs
+++ EIMS/Controllers/FacultyController.cs
@@ -8,48 +8,48 @@ using System.Web.Mvc;
 
 namespace EIMS.Controllers
 {
-	public class FacultyConroller : Controller
-	{
-		private IRepository context = new Repository.Repository();
+    public class FacultyController : Controller
+    {
+        private IRepository context = new Repository.Repository();
 
 
-		public ActionResult GetFaculties()
-		{
-			var facultyList = new List<FacultyViewModel>();
-			var dblst = context.GetFaculties();
-			foreach (var fac in dblst)
-			{
-				FacultyViewModel tmp = new FacultyViewModel()
-				{
-					FacultyID = fac.FacultyID,
-					Name = fac.Name
-				};
-				facultyList.Add(tmp);
-			}
-			return View(facultyList);
-		}
+        public ActionResult GetFaculties()
+        {
+            var facultyList = new List<FacultyViewModel>();
+            var dblst = context.GetFaculties();
+            foreach (var fac in dblst)
+            {
+                FacultyViewModel tmp = new FacultyViewModel()
+                {
+                    FacultyID = fac.FacultyID,
+                    Name = fac.Name
+                };
+                facultyList.Add(tmp);
+            }
+            return View(facultyList);
+        }
 
-		public ActionResult CreateFaculty(FacultyViewModel faculty)
-		{
-			CreateFaculty(faculty);
-			return View(faculty);
-		}
+        public ActionResult CreateFaculty(FacultyViewModel faculty)
+        {
+            CreateFaculty(faculty);
+            return View(faculty);
+        }
 
-		public ActionResult UpdateFaculty(FactorViewModel faculty)
-		{
-			UpdateFaculty(faculty);
-			return View(faculty);
-		}
+        public ActionResult UpdateFaculty(FactorViewModel faculty)
+        {
+            UpdateFaculty(faculty);
+            return View(faculty);
+        }
 
-		public ActionResult DeleteFaculty(int id)
-		{
-			DeleteFaculty(id);
-			return View();
-		}
+        public ActionResult DeleteFaculty(int id)
+        {
+            DeleteFaculty(id);
+            return View();
+        }
 
-		public ActionResult GetFacultyByID(int id)
-		{
-			return View(context.GetFacultyByID(id));
-		}
-	}
+        public ActionResult GetFacultyByID(int id)
+        {
+            return View(context.GetFacultyByID(id));
+        }
+    }
 }
