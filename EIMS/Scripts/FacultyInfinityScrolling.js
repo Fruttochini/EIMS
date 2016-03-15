9c3979624444150676ca7e9460b5eea937e58cf6
zanpaln <zanpaln@i.ua>
2016-03-15 17:51:37 +0200

    Update

---
+++ EIMS/Scripts/FacultyInfinityScrolling.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+
+    var page = 0;
+    var _inCallback = false;
+    function loadItems() {
+        if (page > -1 && !_inCallback) {
+            _inCallback = true;
+            page++;
+
+            $.ajax({
+                type: 'GET',
+                url: '/Faculty/Faculties/' + page,
+                success: function (data, textstatus) {
+                    if (data != '') {
+                        $(".table").append(data);
+                    }
+                    else {
+                        page = -1;
+                    }
+                    _inCallback = false;
+                }
+            });
+        }
+    }
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+
+            loadItems();
+        }
+    });
+})
