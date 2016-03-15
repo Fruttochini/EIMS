5811a9d10076902394e61831470d35d8d5ab4743
Oleksandr <danolex@ukr.net>
2016-03-15 21:17:22 +0200

    subjectController

---
+++ EIMS/Controllers/SubjectController.cs
@@ -0,0 +1,31 @@
+﻿using EIMS.Common;
+using EIMS.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EIMS.Controllers
+{
+    public class SubjectController : Controller
+    {
+        IRepository context;
+
+        public SubjectController()
+        {
+            context = new Repository.Repository();
+        }
+        // GET: Subject
+        public ActionResult GetSubjectList()
+        {
+            var subjList = context.GetSubjects();
+            return View(subjList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
