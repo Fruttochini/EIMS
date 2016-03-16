ab8e42406fa592b780fbe457acdd20238e297287
zanpaln <zanpaln@i.ua>
2016-03-16 19:47:10 +0200

    adding CourseFillViewModel

---
+++ EIMS/Models/CourseFillViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EIMS.Models
+{
+	public class CourseFillViewModel
+	{
+		public int courseID { get; set; }
+		public int subjectID { get; set; }
+
+		public string courseName { get; set; }
+		public string subjectName { get; set; } 
+		public int SubjectHoursPerWeek { get; set; }
+	}
+}
