63d3f6621df0f58be62c765bf700b4723d9b6012
zanpaln <zanpaln@i.ua>
2016-03-16 11:44:43 +0200

    Update

--- EIMS/Controllers/CourseController.cs
+++ EIMS/Controllers/CourseController.cs
@@ -11,9 +11,26 @@ namespace EIMS.Controllers
     public class CourseController : Controller
     {
 		private IRepository context = new Repository.Repository();
-		// GET: Course
-		public ActionResult GetCourse()
+		const int pageSize = 9;
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult GetCourse(int? id)
         {
+			int page = id ?? 0;
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView("GetCourse", GetItemsPerPage(page));
+			}
+			return PartialView(GetItemsPerPage());
+        }
+
+		public object GetItemsPerPage(int page = 0)
+		{
+			var itemToSkip = page * pageSize;
 			var courseList = new List<CourseViewModel>();
 			var dblst = context.GetCourses();
 			foreach (var item in dblst)
@@ -22,12 +39,26 @@ namespace EIMS.Controllers
 				{
 					CourseID = item.CourseID,
 					CourseName = item.CourseName,
-					SubjectByHours = item.SubjectByHours
 				};
 				courseList.Add(cvm);
 			}
-            return View(courseList);
-        }
+			return courseList.OrderBy(c=>c.CourseID).Skip(itemToSkip).Take(pageSize).ToList();
+		}
+
+		public ActionResult CreateCourse()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+
+		public ActionResult CreateCourse(CourseViewModel course)
+		{
+			return View();
+		}
+
 
 
     }
