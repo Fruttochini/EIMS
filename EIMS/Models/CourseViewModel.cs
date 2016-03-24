14a34190d0d75d195c52f28bc5dcac02ca20e331
Oleksandr <danolex@ukr.net>
2016-03-24 13:52:23 +0200

    Nice look of AdminWeb

--- EIMS/Models/CourseViewModel.cs
+++ EIMS/Models/CourseViewModel.cs
@@ -7,78 +7,93 @@ using System.Web.Mvc;
 
 namespace EIMS.Models
 {
-	public class CourseViewModel
-	{
-		public int CourseID { get; set; }
-		public string CourseName { get; set; }
+    public class CourseViewModel
+    {
+        [Display(Name = "ID")]
+        public int CourseID { get; set; }
+        [Display(Name = "Course Name")]
+        public string CourseName { get; set; }
 
-		//public Dictionary<string,int> SubjectByHours { get; set; } 
-	}
+        //public Dictionary<string,int> SubjectByHours { get; set; } 
+    }
 
-	public class CreateEditCourseFillViewModel
-	{
-		public int courseFillID { get; set; }
-		public int courseID { get; set; }
-		public int subjectID { get; set; }
-		public int SubjectHoursPerWeek { get; set; }
-		public string Subject { get; }
+    public class CreateEditCourseFillViewModel
+    {
+        public int courseFillID { get; set; }
+        public int courseID { get; set; }
+        public int subjectID { get; set; }
+        [Display(Name = "Hours/Week")]
+        public int SubjectHoursPerWeek { get; set; }
+        [Display(Name = "Subject")]
+        public string Subject { get; }
 
-		public IEnumerable<SelectListItem> SubjectList { get; set; }
-	}
+        public IEnumerable<SelectListItem> SubjectList { get; set; }
+    }
 
-	public class CourseFillList
-	{
-		public int courseFillID { get; set; }
-		public int courseID { get; set; }
-		public string subjectName { get; set; }
-		public int subjectID { get; set; }
-		public int SubjectHoursPerWeek { get; set; }
-	}
+    public class CourseFillList
+    {
+        public int courseFillID { get; set; }
+        public int courseID { get; set; }
+        public string subjectName { get; set; }
+        public int subjectID { get; set; }
+        public int SubjectHoursPerWeek { get; set; }
+    }
 
-	public class CourseFillViewModel
-	{
-		public CourseViewModel SelectCourse { get; set; }
+    public class CourseFillViewModel
+    {
+        public CourseViewModel SelectCourse { get; set; }
+        [Display(Name = "Subject")]
+        public string SubjectName { get; }
+        [Display(Name = "Hours/Week")]
+        public string SubjectHoursPerWeek { get; }
 
-		public string SubjectName { get; }
-		public string SubjectHoursPerWeek { get; }
+        public IEnumerable<CourseFillList> subjectList { get; set; }
+    }
 
-		public IEnumerable<CourseFillList> subjectList { get; set; }
-	}
+    public class CreateEditGroupCourseViewModel
+    {
+        public int groupCourseID { get; set; }
+        public int courseID { get; set; }
+        public int groupID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start date")]
+        public DateTime? startDate { get; set; }
 
-	public class CreateEditGroupCourseViewModel
-	{
-		public int groupCourseID { get; set; }
-		public int courseID { get; set; }
-		public int groupID { get; set; }
-		[DataType(DataType.Date)]
-		[DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
-		public DateTime? startDate { get; set; }
-		[DataType(DataType.Date)]
-		[DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
-		public DateTime? endDate { get; set; }
-		public string Group { get; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "End date")]
+        public DateTime? endDate { get; set; }
 
-		public IEnumerable<SelectListItem> GroupList { get; set; }
-	}
+        public string Group { get; }
 
-	public class GroupCoursList
-	{
-		public int groupCourseID { get; set; }
-		public int courseID { get; set; }
-		public int groupID { get; set; }
-		public string GroupName { get; set; }
-		public DateTime? startDate { get; set; }
-		public DateTime? endDate { get; set; }
-	}
+        public IEnumerable<SelectListItem> GroupList { get; set; }
+    }
 
-	public class GroupCoursViewModel
-	{
-		public CourseViewModel SelectCourse { get; set; }
+    public class GroupCoursList
+    {
+        public int groupCourseID { get; set; }
+        public int courseID { get; set; }
+        public int groupID { get; set; }
+        [Display(Name = "Group")]
+        public string GroupName { get; set; }
+        [Display(Name = "Start date")]
+        public DateTime? startDate { get; set; }
+        [Display(Name = "End date")]
+        public DateTime? endDate { get; set; }
+    }
 
-		public string GroupName { get; }
-		public string StartDate { get; }
-		public string EndDate { get; }
+    public class GroupCoursViewModel
+    {
+        public CourseViewModel SelectCourse { get; set; }
 
-		public IEnumerable<GroupCoursList> groupList { get; set; }
-	}
+        [Display(Name = "Group")]
+        public string GroupName { get; }
+        [Display(Name = "Start date")]
+        public string StartDate { get; }
+        [Display(Name = "End date")]
+        public string EndDate { get; }
+
+        public IEnumerable<GroupCoursList> groupList { get; set; }
+    }
 }
